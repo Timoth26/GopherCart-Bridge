a12e312930db64085f10eccda5cba5dbf1575291
Tomasz <tomekmich26@gmail.com>
2026-04-16 20:15:21 +0200

    Create and connect product_repo

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -1,10 +1,20 @@
 package domain
 
+import "context"
+
 type Product struct {
-	ID          int64   `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	Stock       int     `json:"stock"`
-	ProviderID  int     `json:"provider_id"`
+	ID          int64   `json:"id"          db:"id"`
+	Name        string  `json:"name"        db:"name"`
+	Description string  `json:"description" db:"description"`
+	Price       float64 `json:"price"       db:"price"`
+	Stock       int     `json:"stock"       db:"stock"`
+	ProviderID  int     `json:"provider_id" db:"provider_id"`
+}
+
+type ProductRepository interface {
+	GetByID(ctx context.Context, id int64) (*Product, error)
+	GetAll(ctx context.Context) ([]Product, error)
+	Create(ctx context.Context, p *Product) error
+	Update(ctx context.Context, p *Product) error
+	Delete(ctx context.Context, id int64) error
 }
